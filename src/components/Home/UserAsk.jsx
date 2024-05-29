28f747b95c9c69b447ac7474637c1e03e85152c5
Abu Hanif <hanifcse90@gmail.com>
2024-05-29 21:31:34 +0600

    Pupoles comment add

---
+++ src/components/Home/UserAsk.jsx
@@ -0,0 +1,33 @@
+const UserAsk = () => {
+  return (
+    <div>
+      <h1>Frequently Asked Questions</h1>
+      <div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-arrow border border-base-300 bg-base-200"
+        >
+          <div className="collapse-title text-xl font-medium">
+            Focus me to see content
+          </div>
+          <div className="collapse-content">
+            <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+          </div>
+        </div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-arrow border border-base-300 bg-base-200"
+        >
+          <div className="collapse-title text-xl font-medium">
+            Focus me to see content
+          </div>
+          <div className="collapse-content">
+            <p>tabIndex={0} attribute is necessary to make the div focusable</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserAsk;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../components/Home/Banner";
 import CategoryBanner from "../components/Home/CategoryBanner";
+import UserAsk from "../components/Home/UserAsk";
 
 const Home = () => {
   return (
     <>
       <CategoryBanner />
       <Banner />
+      <UserAsk />
     </>
   );
 };
